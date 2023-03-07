69fc965d066554e8a7107c0673fa6f3a4400576a
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-03-07 21:26:12 +0000

    Complete kata5 test and solution

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const exactTime = distance / speed;
+    const roundedUpTime = Math.ceil(exactTime/0.5) * 0.5;
+    return `I should be there in ${roundedUpTime} hours.`
+};
 
 module.exports = reachDestination;
