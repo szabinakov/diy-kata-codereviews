296d5f63ecc9d4414ef9f9561586fd85397f99fe
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-03-08 20:53:37 +0000

    solved numberToReversedDigits test and function

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,7 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits([1,2,3])).toEqual([3,2,1]);
+  });
 });
