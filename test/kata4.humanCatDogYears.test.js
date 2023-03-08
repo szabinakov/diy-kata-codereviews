e4bf83a570bdc7f638ca88367a3918dddc160b4e
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-03-08 21:47:08 +0000

    solved the humancatdogyears test and function

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,13 @@
 const { humanCatDogYears } = require("../src");
+    
+describe("humanCatDogYears", () => {
+    test("returning array of human, cat and dog years", () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);    
+    });
+    test("returning array of human, cat and dog years", () => {
+        expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+    });
+});
 
-// Look Ma, no handlebars!!!
+
+
