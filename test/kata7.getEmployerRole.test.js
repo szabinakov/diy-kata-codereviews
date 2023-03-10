1132727ae165f3a3213ffd5a58e97f1784ae7b6c
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-03-10 06:55:15 +0000

    Complete kata7 test and solution

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,10 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  it("returns the employee's role in the company", () => {
+  const employees = [{name: 'Satti',role: 'Developer'}, {name: 'Jenny',role: 'Sales Associate'}, {name: 'Javid',role: 'Human Recommended Reading Assistant'}];
+  expect(getEmployerRole('Satti', employees)).toEqual('Developer');
+  expect(getEmployerRole('Jenny', employees)).toEqual('Sales Associate');
+  expect(getEmployerRole('Javid', employees)).toEqual('Human Recommended Reading Assistant');
+  });
 });
