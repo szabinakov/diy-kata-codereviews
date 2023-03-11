58240c925817f3be0e443e25c30b45560b87e5a1
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-03-11 17:39:35 +0000

    passed the employee role test and function

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,5 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  return employees.find(employee => employee.name == employeeName).role
+};
 
 module.exports = getEmployerRole;
