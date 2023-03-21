afb79f3b4787557dfcd3176457c8e7968f84b6f5
ThePahp <asif_islam@outlook.com>
2023-03-21 19:12:17 +0000

    getEmployerRole test completed

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,15 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  const employees = [
+    { name: "Asif", role: "Electrical Engineer" },
+    { name: "Adeel", role: "Mechanical Engineer" },
+    { name: "Liam", role: "BIM Technician" },
+  ];
+  it("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Asif", employees)).toBe(
+      "Electrical Engineer"
+    );
+    expect(getEmployerRole("Liam", employees)).toBe("BIM Technician");
+  });
 });
