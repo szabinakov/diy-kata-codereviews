3b7d49833518fb8988f843561d80b88a1043a30b
ThePahp <asif_islam@outlook.com>
2023-03-21 19:21:42 +0000

    joinNames test completed

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -4,11 +4,11 @@ const { joinNames } = require("../src");
 describe("joinNames", () => {
   it("returns string of names, seperated by commas and an ampersand", () => {
     expect(
-      joinNames([{ name: "Asif" }, { name: "Liam" }, { name: "Emily" }])
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Magie" }])
     ).toBe("Asif, Liam & Emily");
     expect(
-      joinNames([{ name: "Hamaad" }, { name: "Remon" }, { name: "Adeel" }])
-    ).toBe("Hamaad, Remon & Adeel");
+      joinNames([{ name: "Asif" }, { name: "Remon" }, { name: "Adeel" }])
+    ).toBe("Asif, Remon & Adeel");
   });
 
 });
