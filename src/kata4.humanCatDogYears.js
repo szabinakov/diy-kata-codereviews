0cbfd1cad56f8e925b14ac981fc11ef175d161aa
ThePahp <asif_islam@outlook.com>
2023-03-21 16:05:29 +0000

    humanCatDogYears test completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,17 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (years) => {
+    let catYears = 0;
+    let dogYears = 0;
+    if (years == 1) {
+        catYears = 15;
+        dogYears = 15;
+    } else if (years == 2) {
+        catYears = 24;
+        dogYears = 24;
+    } else {
+        catYears = 24 + 4 * (years - 2);
+        dogYears = 24 + 5 * (years - 2);
+    };
+    return [years, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
