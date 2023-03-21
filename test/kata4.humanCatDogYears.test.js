f27ed5d61cf67bb55a50c52ead9981fb1d0648fd
ThePahp <asif_islam@outlook.com>
2023-03-21 16:06:01 +0000

    humanCatDogYears test completed

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
+
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    test("returning array of human, cat and dog years", () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    });
+    test("returning array of human, cat and dog years", () => {
+        expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+    });
+});
