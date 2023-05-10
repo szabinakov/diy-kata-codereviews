5ede64a4ef884a8358152625ba2dc99ea8be1886
Max Staite <max.staite2004@gmail.com>
2023-05-10 17:37:51 +0100

    solved join names

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,18 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const name1 = {
+      name: "Bart",
+    };
+    const name2 = {
+      name: "Lisa",
+    };
+    const name3 = {
+      name: "Maggie",
+    };
+    expect(joinNames([name1, name2, name3])).toBe("Bart, Lisa & Maggie");
+    expect(joinNames([name1, name2])).toBe("Bart & Lisa");
+    expect(joinNames([name2, name3])).toBe("Lisa & Maggie");
+  });
 });
